61a5b17dec9753401cb7de1799c1a71e1a22635b
karan100010 <karanjoshi1993@gmail.com>
2019-01-11 10:36:12 +0530

    fixing bug #4

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- 
+import os
+from setuptools import setup
+#from distutils.core import setup
 
-from setuptools  import setup
+
 setup(
   name = 'xetrapal',         # How you named your package folder (MyLib)
   packages = ['xetrapal'],   # Chose the same as "name"
@@ -12,8 +15,8 @@ setup(
   author_email = 'listener@hackergram.org',      # Type in your E-Mail
   url = 'https://github.com/hackergram/xetrapal',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/hackergram/xetrapal/archive/0.0.tar.gz',    # I explain this later on
-  keywords = ['automation', 'orchestration' ],   # Keywords that define your package 
- install_requires=[            # I get to this in a second
+  keywords = ['automation', 'orchestration' ],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
           'python_telegram_bot==7.0.1',
 'twython==3.6.0',
 'tweepy==3.6.0',
@@ -28,22 +31,6 @@ setup(
 'beautifulsoup4==4.6.3',
 'configparser==3.5.0',
 'oauth2client==4.1.3'],
-setup_requires=[            # I get to this in a second
-          'python_telegram_bot==7.0.1',
-'twython==3.6.0',
-'tweepy==3.6.0',
-'colored==1.3.5',
-'Pygments==2.1',
-'pygsheets==1.1.4',
-'thespian==3.9.2',
-'Pykka==1.2.1',
-'selenium==3.8.1',
-'coloredlogs==9.0',
-'pandas==0.17.1',
-'beautifulsoup4==4.6.3',
-'configparser==3.5.0',
-'oauth2client==4.1.3'],
-
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
@@ -53,3 +40,5 @@ setup_requires=[            # I get to this in a second
     'Programming Language :: Python :: 2.7',	
   ],
 )
+op=os.popen("sudo -H pip install -r requirements.txt").read()
+print op
