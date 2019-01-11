7f7465181330bb7901cd0c0815d592097821ce41
karan100010 <karanjoshi1993@gmail.com>
2019-01-11 11:22:24 +0530

    fixing bug #4

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ import os
 from setuptools import setup
 #from distutils.core import setup
 
-
 setup(
   name = 'xetrapal',         # How you named your package folder (MyLib)
   packages = ['xetrapal'],   # Chose the same as "name"
@@ -16,21 +15,7 @@ setup(
   url = 'https://github.com/hackergram/xetrapal',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/hackergram/xetrapal/archive/0.0.tar.gz',    # I explain this later on
   keywords = ['automation', 'orchestration' ],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'python_telegram_bot==7.0.1',
-'twython==3.6.0',
-'tweepy==3.6.0',
-'colored==1.3.5',
-'Pygments==2.1',
-'pygsheets==1.1.4',
-'thespian==3.9.2',
-'Pykka==1.2.1',
-'selenium==3.8.1',
-'coloredlogs==9.0',
-'pandas==0.17.1',
-'beautifulsoup4==4.6.3',
-'configparser==3.5.0',
-'oauth2client==4.1.3'],
+
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
